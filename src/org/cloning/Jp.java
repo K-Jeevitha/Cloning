7a6b59eded61f5271dcc3fb7ed230d1b8c3c7519
K-Jeevitha <jeevithak94@gmail.com>
2022-04-03 09:54:01 +0530

    cloning project added

---
+++ src/org/cloning/Jp.java
@@ -0,0 +1,12 @@
+package org.cloning;
+
+public class Jp {
+	public static void main(String[] args) {
+		int b=5,c=10;
+		int a=b+c;
+			System.out.println(a);
+	}
+	
+
+
+}
---
+++ src/org/cloning/jv.java
@@ -0,0 +1,8 @@
+package org.cloning;
+
+public class jv {
+	public static void main(String[] args) {
+		System.out.println("dummy");
+	}
+
+}
